9747b8ff98ee7748eee21a59eec2ff8f4ef38169
xogus303 <rlaxogus9510@gmail.com>
2020-09-15 23:24:22 +0900

    Create Customer.js

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
-
+asd
 In the project directory, you can run:
 
 ### `yarn start`
--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '951003',
+    'gender': '남자',
+    'job': '소방관'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': '고길동',
+    'birthday': '960528',
+    'gender': '남자',
+    'job': '관리자'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': '그레이트',
+    'birthday': '909090',
+    'gender': '남자',
+    'job': '장로'
+  },
+]
 
 class App extends Component {
   render(){
     return (
-      <div className='gray-background'>
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })}
       </div>
     )
   }
