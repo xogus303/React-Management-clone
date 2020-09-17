b869cea76faeb7c1752e8ec9e4ee9398e9e0621b
xogus303 <rlaxogus9510@gmail.com>
2020-09-17 22:53:44 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,17 +18,32 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
+/*
+react component 실행 순서
+1) constructor()
+2) componentWillMount()
+3) render()
+4) componentDidMount()
+
+props or state => shouldcomponentDidMount()
+
+*/
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0,
   }
 
   componentDidMount(){ // 모든 컴포넌트가 마운트 완료 되었을 때
+    this.timer = setInterval(this.progress, 20)
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err))
@@ -37,6 +53,11 @@ class App extends Component {
     const response = await fetch('/api/customers')
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 })
   }
   
   render(){
@@ -68,7 +89,13 @@ class App extends Component {
                     job={c.job}
                   />
                 )
-              }) : null}
+              }) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+              )}
           </TableBody>
         </Table>
       </Paper>
